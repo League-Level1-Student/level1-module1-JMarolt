01c341d8b0fdb976e01800f6df9acce6a95445e0
audreychan <league@wts14-4.attlocal.net>
2019-03-10 15:54:42 -0700

    finished whole module in 10 minutes yikes

--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -1,18 +1,32 @@
 
-
 public class RaceCarRunner {
+
 	public static void main(String[] args) {
 		/* Do the following things without changing the RaceCar class */
-		
+
 		// 1. Create a RaceCar and place it in 5th position
-	
+
+		RaceCar car = new RaceCar("Fortnite", 5);
+
 		// 2. Print the RaceCar's position in the race
 
+		car.getPositionInRace();
+
 		// 3. Crash the RaceCar
-		
+
+		car.crash();
+
 		// 4. If the car is damaged. Bring it in for a pit stop.
 
+		if (car.damaged == true) {
+			car.pit();
+		}
+
 		// 5. Help the car move into first place.
+
+		while (car.getPositionInRace() != 1) {
+			car.overtake();
+		}
 
 	}
 }
